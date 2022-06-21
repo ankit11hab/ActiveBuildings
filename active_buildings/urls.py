4f0af0584022f03b19895cf089ff2f9eb8da2b01
Ankit Guha <75986219+ankit11hab@users.noreply.github.com>
2022-06-21 19:42:47 +0530

    Update urls.py

--- active_buildings/urls.py
+++ active_buildings/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf.urls import url
-from django.views.static import serve
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('', include('base.urls'))
 ]
